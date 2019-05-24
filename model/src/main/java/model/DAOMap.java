80e9532f610aca3444b4ed0844b3f29435c913d1
WEIMANN Théo <31115344+wealw@users.noreply.github.com>
2019-05-24 13:11:41 +0200

    Add the DAOMap class and corresponding tests.

---
+++ model/src/main/java/model/DAOMap.java
@@ -0,0 +1,117 @@
+package model;
+
+import entity.BehaviourImmovable;
+import entity.Entity;
+import entity.Wall;
+
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ * The Class DAOMap.
+ * Used to get the map from a distant or local DB using the "../../resources/model.properties" files to get database log in.
+ *
+ * @author Théo Weimann
+ */
+
+class DAOMap {
+    /**
+     * The connection parameter inherited from the "../../resources/model.properties" and get by the Abstract class Connection.
+     */
+    private final Connection connection;
+    /**
+     * The size on the X axis of a map.
+     */
+    private int sizeX;
+    /**
+     * The size on the Y axis of a map.
+     */
+    private int sizeY;
+
+    /**
+     * Instantiates a new DAO hello world.
+     *
+     * @param connection the connection
+     * @throws SQLException the SQL exception
+     */
+    public DAOMap(final Connection connection) throws SQLException {
+        this.connection = connection;
+    }
+
+
+    /**
+     * Import a map from a distant DB.
+     *
+     * @param id The id of the map you want to import.
+     * @return The map that you asked for by her ID
+     */
+    public Entity[][] find(final int id) {
+        this.setSize(id);
+        Entity[][] map = new Entity[this.sizeX][this.sizeY];
+        try {
+            final String sql = "{call getMap(?)}";
+            final CallableStatement call = this.connection.prepareCall(sql);
+            call.setInt(1, id);
+            call.execute();
+            final ResultSet resultSet = call.getResultSet();
+            if (resultSet.first()) {
+                while (!resultSet.isAfterLast()) {
+                    switch (resultSet.getString(3)) {
+                        case "T":
+                            map[resultSet.getInt(1) - 1][resultSet.getInt(2) - 1] = new Wall(new BehaviourImmovable());
+                    }
+
+                    resultSet.next();
+                }
+
+            }
+            return map;
+        } catch (final Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    /**
+     * Get the dimension of a map by searching her from her ID on a DB.
+     *
+     * @param id The id of the map you want to import.
+     */
+    private void setSize(final int id) {
+
+        try {
+            final String sql = "{call getDimension(?)}";
+            final CallableStatement call = this.connection.prepareCall(sql);
+            call.setInt(1, id);
+            call.execute();
+            final ResultSet resultSet = call.getResultSet();
+            if (resultSet.first()) {
+                this.sizeX = resultSet.getInt(1);
+                this.sizeY = resultSet.getInt(2);
+            }
+
+        } catch (final Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Getter of sizeX.
+     *
+     * @return Return the sizeX parameter of a DAO Object.
+     */
+    public int getSizeX() {
+        return sizeX;
+    }
+
+    /**
+     * Getter of sizeY.
+     *
+     * @return Return the sizeY parameter of a DAO Object.
+     */
+    public int getSizeY() {
+        return sizeY;
+    }
+}
---
+++ model/src/test/java/model/DAOMapTest.java
@@ -0,0 +1,79 @@
+package model;
+
+import entity.Entity;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author Théo Weimann
+ */
+public class DAOMapTest {
+    private DAOMap daoMap;
+
+    /**
+     * Instantiate a new DOAMap object.
+     *
+     * @throws Exception
+     */
+    @Before
+    public void setUp() throws Exception {
+        daoMap = new DAOMap(DBConnection.getInstance().getConnection());
+    }
+
+
+    /**
+     * Check if the DB is loadable.
+     *
+     * @throws Exception
+     */
+    @Test
+    public void findConnectionTest() throws Exception {
+        int mapId = 1;
+        try {
+            this.daoMap.find(mapId);
+        } catch (final Exception e) {
+            throw new Exception("Can't link to Database.");
+        }
+
+
+    }
+
+    /**
+     * Check if the DB is loadable.
+     *
+     * @throws Exception
+     */
+    @Test
+    public void setSizeConnectionTest() throws Exception {
+        int mapId = 1;
+        try {
+            this.daoMap.find(mapId);
+        } catch (final Exception e) {
+            throw new Exception("Can't link to Database.");
+        }
+    }
+
+    /**
+     * Check if the find method can load a map from a DB.
+     */
+    @Test
+    public void findLoadTest() {
+        int idMap = 1;
+        Entity[][] result = daoMap.find(idMap);
+        assertNotNull(result);
+    }
+
+    /**
+     * Check if the method
+     */
+    @Test
+    public void getSizeLoadTest() {
+        int mapSize = 25;
+        assertEquals(mapSize, this.daoMap.getSizeX());
+        assertEquals(mapSize, this.daoMap.getSizeY());
+    }
+
+}
