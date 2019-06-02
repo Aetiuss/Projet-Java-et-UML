9d9c80d2c3dda6682c4d2178947725a8a1586f93
Luc Antoni <l.antoni@hotmail.fr>
2019-06-02 15:07:01 +0200

    Added commentaries + MAJ diagrammes

--- model/src/main/java/model/entity/Ennemy.java
+++ model/src/main/java/model/entity/Ennemy.java
@@ -33,5 +33,4 @@ public class Ennemy extends Entity {
             e.printStackTrace();
         }
     }
-    
 }
--- model/src/main/java/model/entity/Exit.java
+++ model/src/main/java/model/entity/Exit.java
@@ -19,10 +19,18 @@ public class Exit extends Entity {
         super();
     }
 
+    /**
+     * @return the instance of the singleton
+     */
     static public Exit getInstance() {
         return instance;
     }
 
+    /**
+     *
+     *
+     * @return the instance of the singleton if not created with coordonate
+     */
     static public Exit getInstance(int x, int y, Entity[][] map) {
         instance.x = x;
         instance.y = y;
@@ -30,11 +38,21 @@ public class Exit extends Entity {
         return instance;
     }
 
+    /**
+     *
+     *
+     * @return the sprite
+     */
     @Override
     public char getSprite() {
         return sprite;
     }
 
+    /**
+     *
+     *
+     * load the sprite
+     */
     @Override
     public void loadImage() {
 
@@ -48,14 +66,29 @@ public class Exit extends Entity {
         }
     }
 
+    /**
+     *
+     *
+     * get the Score
+     */
     public int getScore() {
         return score;
     }
 
+    /**
+     *
+     *
+     * set the Score
+     */
     public void setScore(int score) {
         this.score = score;
     }
 
+    /**
+     *
+     *
+     * check if the exit can open
+     */
     public void exitOpen() {
         if (score == 100) {
             this.exitIsOpen = true;
@@ -65,6 +98,11 @@ public class Exit extends Entity {
         }
     }
 
+    /**
+     *
+     *
+     * end the game if the score is high enough
+     */
     public void end() {
         if (this.exitIsOpen == true) {
             BoardFrame.end();
