84f1930b6ebb73da48b244bf22d73e6f3380c22f
Théo Weimann <theo.weimann.priv@gmail.com>
2019-06-03 09:17:48 +0200

    Resolving bugs on ennemyTest.

--- model/src/test/java/model/entity/EnnemyTest.java
+++ model/src/test/java/model/entity/EnnemyTest.java
@@ -11,22 +11,29 @@ public class EnnemyTest
     
     Entity[][] map;
     Ennemy     ennemy;
-    Wall       wall1, wall2, wall3;
+    Wall wall1, wall2, wall3, wall4, wall5, wall6;
     Empty empty;
     @Before
     public void setUp() throws Exception
     {
-        map = new Entity[3][3];
+        map = new Entity[4][4];
         ennemy = new Ennemy(1, 1, map);
         wall1 = new Wall(0, 1, map);
         wall2 = new Wall(1, 0, map);
         wall3 = new Wall(1, 2, map);
+        wall4 = new Wall(2, 2, map);
+        wall5 = new Wall(2, 0, map);
+        wall6 = new Wall(3, 1, map);
         empty = new Empty(2, 1, map);
         map[1][1] = ennemy;
         map[0][1] = wall1;
         map[1][0] = wall2;
         map[1][2] = wall3;
         map[2][1] = empty;
+        map[2][2] = wall4;
+        map[2][0] = wall5;
+        map[3][1] = wall6;
+
     }
     @After
     public void tearDown() throws Exception
